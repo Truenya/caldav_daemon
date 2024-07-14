698a89ba03f32a7bf94a772716d45917f947f82a
Крылов Александр <aleksandr.krylov@hyperus.team>
2024-07-14 11:23:04 +0300

    refactor: python script

--- caldav-fetch.py
+++ caldav-fetch.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-from icalendar import Calendar
-import caldav
 import json
 from os import environ as env
 
+import caldav
+
 username = env["CALDAV_USERNAME"]
 password = env["CALDAV_PASSWORD"]
-url      = env["CALDAV_URL"]
+url = env["CALDAV_URL"]
 caldav_url = f"https://{url}/{username}/"
 headers = {}
 
@@ -16,31 +16,36 @@ def fetch_and_print():
         url=caldav_url,
         username=username,
         password=password,
-        headers=headers,  # Optional parameter to set HTTP headers on each request if needed
+        # Optional parameter to set HTTP headers on each request if needed
+        headers=headers,
     ) as client:
-        print_calendar_events_json(client.principal().calendars())
+        print_calendars_demo(client.principal().calendars())
 
 
-def print_calendar_events_json(calendars):
+def print_calendars_demo(calendars):
     if not calendars:
         return
     events = []
     for calendar in calendars:
-        for eventraw in calendar.events():
-            for component in Calendar.from_ical(eventraw._data).walk():
+        for event in calendar.events():
+            for component in event.icalendar_instance.walk():
                 if component.name != "VEVENT":
                     continue
-                cur = {}
-                cur['calendar'] = f'{calendar}'
-                cur['summary'] = component.get('summary')
-                cur['description'] = component.get('description')
-                cur['start'] = component.get('dtstart').dt.strftime('%m/%d/%Y %H:%M')
-                endDate = component.get('dtend')
-                if endDate and endDate.dt:
-                    cur['end'] = endDate.dt.strftime('%m/%d/%Y %H:%M')
-                cur['datestamp'] = component.get('dtstamp').dt.strftime('%m/%d/%Y %H:%M')
-                events.append(cur)
+                events.append(fill_event(component, calendar))
     print(json.dumps(events, indent=2, ensure_ascii=False))
+
+
+def fill_event(component, calendar) -> dict[str, str]:
+    cur = {}
+    cur["calendar"] = f"{calendar}"
+    cur["summary"] = component.get("summary")
+    cur["description"] = component.get("description")
+    cur["start"] = component.get("dtstart").dt.strftime("%m/%d/%Y %H:%M")
+    endDate = component.get("dtend")
+    if endDate and endDate.dt:
+        cur["end"] = endDate.dt.strftime("%m/%d/%Y %H:%M")
+    cur["datestamp"] = component.get("dtstamp").dt.strftime("%m/%d/%Y %H:%M")
+    return cur
 
 
 if __name__ == "__main__":
